b23278ca94e8be21f926a68c6a86e6ea3b860f7f
Logan Lin <linyanwd213@gmail.com>
2018-04-06 23:02:34 +0800

    Adjust sql files

--- data_fetch/table_for_fetch.sql
+++ data_fetch/table_for_fetch.sql
@@ -9,4 +9,17 @@ create table bj_current_meo_grid
 	wind_direction float null,
 	wind_speed float null,
 	primary key (stationName, utctime)
+);
+
+create table bj_current_aq
+(
+	stationid varchar(128) not null,
+	utctime datetime not null,
+	`PM2.5` int null,
+	PM10 int null,
+	NO2 int null,
+	CO double null,
+	O3 int null,
+	SO2 int null,
+	primary key (stationid, utctime)
 )
